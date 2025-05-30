0d6da49e134ab063be2c0ff67c34d44f3554a3f7
AxelSantino <santinopattenden@gmail.com>
2025-05-30 01:39:45 -0300

    Advanced logic, mid problems in UI

--- tp3/src/interfaz/Menu.java
+++ tp3/src/interfaz/Menu.java
@@ -2,10 +2,12 @@ package interfaz;
 
 import java.awt.EventQueue;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Menu extends JFrame {
 
@@ -36,9 +38,19 @@ public class Menu extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-
 		setContentPane(contentPane);
-		contentPane.setLayout(new GridLayout(1, 0, 0, 0));
-	}
+		contentPane.setLayout(null);
+		setLocationRelativeTo(null);
+		JButton btnGrillaAleatoria = new JButton("Grilla Aleatoria");
+		btnGrillaAleatoria.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				new TableroUI();
+				setVisible(true);
+				dispose();
+			}
+		});
+		btnGrillaAleatoria.setBounds(157, 79, 123, 23);
+		contentPane.add(btnGrillaAleatoria);
 
+	}
 }
--- tp3/src/interfaz/TableroUI.java
+++ tp3/src/interfaz/TableroUI.java
@@ -2,46 +2,41 @@ package interfaz;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.EventQueue;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.Window;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.util.Random;
+
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JMenuBar;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import logica.TableroElectronico;
 
 public class TableroUI extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private Random random;
 	private JButton[][] botones;
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					TableroUI frame = new TableroUI();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	private TableroElectronico tablero;
 
-	/**
-	 * Create the frame.
-	 */
 	public TableroUI() {
-		 botones = new JButton[3][4];
+									//CAMBIAR A LOGICA EL RANDOMIZER
+
+		random = new Random();
+		int x = 3;  //harcodeado aproposito para que siempre sume uno mas al X y sea par
+		;// de 1 a 10
+		int y = 3;
+		;
+		if ((x * y) % 2 != 0) { // ESTO TIENE QUE USAR VERIFICARPARIDADGRILLA, CAMBIAR!!!!!!!!!!!!!!!!!!!!!
+			x++; // Suma una fila
+		}
+		tablero = new TableroElectronico(x, y);
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(Window.HEIGHT / 3, Window.WIDTH / 3, 500, 500);
 
@@ -52,74 +47,43 @@ public class TableroUI extends JFrame {
 		menuBar.setBorderPainted(false);
 
 		setJMenuBar(menuBar);
-		menuBar.setLayout(new java.awt.FlowLayout(FlowLayout.CENTER, 60, 1));
-
-		
-
-		JButton btnNewButton = new JButton("Volver");
-		btnNewButton.setFont(new Font("Dialog", Font.BOLD | Font.ITALIC, 13));
-		menuBar.add(btnNewButton);
-		btnNewButton.setBorderPainted(false);
-		btnNewButton.setBackground(null); 
-		btnNewButton.setBorderPainted(false); 
-		btnNewButton.setFocusPainted(false); 
-		btnNewButton.setContentAreaFilled(false);
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			
-			
-				dispose();
-			}
-		});
-		
-
-		JButton btnNewButton_2 = new JButton("Salir   ");
-		btnNewButton_2.setFont(new Font("Dialog", Font.BOLD | Font.ITALIC, 12));
-		menuBar.add(btnNewButton_2);
-		btnNewButton_2.setBorderPainted(false);
-		btnNewButton_2.setBackground(null); 
-		btnNewButton_2.setBorderPainted(false); 
-		btnNewButton_2.setFocusPainted(false); 
-		btnNewButton_2.setContentAreaFilled(false);
-		btnNewButton_2.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				dispose();
-			}
-		});
+		menuBar.setLayout(new FlowLayout(FlowLayout.CENTER, 60, 1));
 
 		contentPane = new JPanel();
 
 		contentPane.setBorder(new EmptyBorder(4, 4, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0)); 
+		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 
-		
 		JPanel panel = new JPanel();
-		panel.setLayout(new GridLayout(3, 4, 0, 0)); 
+		panel.setLayout(new GridLayout(x, y, 0, 0));
 		contentPane.add(panel, BorderLayout.CENTER);
-		
-	  
 
-	    contentPane.add(panel, BorderLayout.CENTER);
-	
-	    
-        setLocationRelativeTo(null);
-        setVisible(true);
-		Comienzo( panel);
+		contentPane.add(panel, BorderLayout.CENTER);
+
+		setLocationRelativeTo(null);
+		setVisible(true);
+		botones = Comienzo(panel, tablero, x, y);
+
 	}
 
-	private void Comienzo( JPanel panel) {
+	private JButton[][] Comienzo(JPanel panel, TableroElectronico tablero, int x, int y) {
+		botones = new JButton[x][y];
 
-		
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 4; j++) {
-				
+		for (int i = 0; i < x; i++) {
+			for (int j = 0; j < y; j++) {
+				int valor = random.nextInt(1) + 1;
+				tablero.setearValorTablero(i, j, valor);
+
 				botones[i][j] = new JButton("");
-				botones[i][j].putClientProperty("x", i); 
-				botones[i][j].putClientProperty("y", j); 
+				botones[i][j].putClientProperty("x", i);
+				botones[i][j].putClientProperty("y", j);
 				botones[i][j].setBorder(BorderFactory.createLineBorder(Color.GRAY, 1));
 				botones[i][j].setEnabled(false);
 				panel.add(botones[i][j]);
 			}
 		}
-	}}
+
+		return botones;
+	}
+}
